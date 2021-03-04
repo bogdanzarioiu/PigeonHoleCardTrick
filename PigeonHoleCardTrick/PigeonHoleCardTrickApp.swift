7e663f662b33cfe3d412c41dfd8d6f26381ed4cf
Bogdan Zarioiu <bogdanzarioiu@gmail.com>
2021-03-04 23:00:27 +0100

    Initial Commit

---
+++ PigeonHoleCardTrick/PigeonHoleCardTrickApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigeonHoleCardTrickApp.swift
+//  PigeonHoleCardTrick
+//
+//  Created by Bogdan on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct PigeonHoleCardTrickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
