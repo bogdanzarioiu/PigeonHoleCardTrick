0ddfba89501da5e376554e1131611175e286d69e
Bogdan Zarioiu <bogdanzarioiu@gmail.com>
2021-03-06 18:18:52 +0100

    Adjusted the UI for the card selection view.

--- PigeonHoleCardTrick/PlayingCardPickerView.swift
+++ PigeonHoleCardTrick/PlayingCardPickerView.swift
@@ -8,15 +8,13 @@
 import SwiftUI
 
 struct PlayingCardPickerView: View {
-    let column1 = ["A♣️", "2♣️", "3♣️", "4♣️", "5♣️", "6♣️", "7♣️",
-                   "8♣️", "9♣️", "10♣️", "J♣️", "Q♣️","K♣️"]
-    let column2 = ["A❤️", "2❤️", "3❤️", "4❤️", "5❤️", "6❤️", "7❤️",
-                   "8❤️", "9❤️", "10❤️", "J❤️", "Q❤️","K❤️"]
-    let column3 = ["A♠️", "2♠️", "3♠️", "4♠️", "5♠️", "6♠️", "7♠️",
-                   "8♠️", "9♠️", "10♠️", "J♠️", "Q♠️","K♠️"]
-    let column4 = ["A♦️", "2♦️", "3♦️", "4♦️", "5♦️", "6♦️", "7♦️",
-                   "8♦️", "9♦️", "10♦️", "J♦️", "Q♦️","K♦️"]
-    
+    let column1 = ["A", "2", "3", "4", "5", "6", "7",
+                   "8", "9", "10", "J", "Q","K"]
+    @State private var didSelectValue = false
+
+    @State var suits = ["♣️", "❤️", "♠️", "♦️"]
+    @Binding var selectedValue: String
+    @Binding var selectedSuit: String
     
     
     let layout = [
@@ -24,32 +22,40 @@ struct PlayingCardPickerView: View {
     ]
     
     var body: some View {
-        LazyHGrid(rows: layout, spacing: 20) {
-            ForEach(column1, id: \.self) { item in
-                Text(item)
-                
+        VStack {
+            Text("Select the value of the card")
+            LazyVGrid(columns: layout, spacing: 20) {
+                ForEach(column1, id: \.self) { item in
+                    Button(action: {
+                        
+                    }, label: {
+                        Text(item)
+                            
+                    })
+                    .disabled(didSelectValue)
+                    
+                    
+                    
+                }
+
+            }.padding()
+            //Text("And now select the suit")
+            Picker("And now select the suit", selection: $selectedSuit) {
+                Text(suits[0])
+                Text(suits[1])
+                Text(suits[2])
+                Text(suits[3])
             }
-            Spacer()
-            ForEach(column2, id: \.self) { item in
-                Text(item)
-                
-            }
-            Spacer()
-            ForEach(column3, id: \.self) { item in
-                Text(item)
-                
-            }
-            Spacer()
-            ForEach(column4, id: \.self) { item in
-                Text(item)
-                
-            }
+            .pickerStyle(MenuPickerStyle())
+            .opacity(didSelectValue ? 1.0 : 0.2)
+            
         }
     }
+    
 }
 
 struct PlayingCardPickerView_Previews: PreviewProvider {
     static var previews: some View {
-        PlayingCardPickerView()
+        PlayingCardPickerView(selectedValue: .constant(""), selectedSuit: .constant(""))
     }
 }
